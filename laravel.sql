efdec10346853ec7278af4ff49c2ce06c4288a52
DiazDeLosSantosJimena <al222110707@gmail.com>
2023-03-06 20:34:38 -0600

    Base de datos actualizada

--- laravel.sql
+++ laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-03-2023 a las 03:30:51
+-- Tiempo de generación: 07-03-2023 a las 03:33:25
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -121,9 +121,9 @@ CREATE TABLE `tb_areas` (
 --
 
 INSERT INTO `tb_areas` (`id_area`, `clave`, `nombre`, `descripcion`, `foto`, `activo`, `id_registro`, `created_at`, `updated_at`) VALUES
-(1, '1', 'ABOGADO GENERAL ', 'ABOGADO GENERAL ', 'cuervo.png	', 0, 1, '2023-03-01 01:24:52', '2023-03-01 01:24:54'),
-(2, '2', 'ORGANO INTERNO DE CONTROL', 'ORGANO INTERNO DE CONTROL\r\n', 'cuervo.png\r\n', 0, 2, '2023-03-01 01:25:29', '2023-03-01 01:25:33'),
-(3, '3', 'UIPPE', 'UNIDAD DE INFROMACION, PLANEACION, PROGRAMACION  Y EVALUACION', 'cuervo.png', 0, 3, '2023-03-01 01:26:59', '2023-03-01 01:27:02'),
+(1, '1', 'ABOGADO GENERAL', 'ABOGADO GENERAL', 'cuervo.png', 0, 1, '2023-03-01 01:24:52', '2023-03-07 02:47:50'),
+(2, '2', 'ORGANO INTERNO DE CONTROL', 'ORGANO INTERNO DE CONTROL', 'cuervo.png', 1, 2, '2023-03-01 01:25:29', '2023-03-07 02:27:17'),
+(3, '3', 'UIPPE', 'UNIDAD DE INFROMACION, PLANEACION, PROGRAMACION  Y EVALUACION', 'cuervo.png', 1, 3, '2023-03-01 01:26:59', '2023-03-07 04:53:13'),
 (4, '4', 'SECRETARIA ACADEMICA', 'SECRETARIA ACADEMICA', 'cuervo.png', 0, 4, '2023-03-01 01:27:43', '2023-03-01 01:27:46'),
 (5, '5', 'DESARROLLO Y FORTALICIMIENTO ACADEMICO', 'DESARROLLO Y FORTALECIMIENTO ACADEMICO 	', 'cuervo.png', 0, 5, '2023-03-01 01:28:39', '2023-03-01 01:28:42'),
 (6, '6', 'SECRETARIA DE VINCULACION', 'SECRETARIA DE VINCULACION', 'cuervo.png', 0, 6, '2023-03-01 01:29:26', '2023-03-01 01:29:30'),
@@ -132,16 +132,17 @@ INSERT INTO `tb_areas` (`id_area`, `clave`, `nombre`, `descripcion`, `foto`, `ac
 (9, '9', 'DEPARTAMENTO DE DESEMPEÑO DE EGRESADOS ', 'DEPARTAMENTO DE DESEMPEÑO DE EGRESADOS', 'cuervo.png', 0, 9, '2023-03-01 01:33:36', '2023-03-01 01:33:38'),
 (10, '10', 'CEPIMUTVTOL', 'CENTRO DE DESAROLLO DE NEGOCIOS/ CENTRO DE PROTECCION DE INVENCIONES Y MARCAS', 'cuervo.png', 0, 10, '2023-03-01 01:34:39', '2023-03-01 01:34:41'),
 (11, '11', 'COORDINACION DE LA ENTIDAD DE CERTIFICACION Y EVAL', 'COORDINACION DE LA ENTIDAD DE CERTIFICACION Y EVALUACION CONOCER', 'cuervo.png', 0, 11, '2023-03-01 01:35:35', '2023-03-01 01:35:37'),
-(12, '12', 'DIRECCION DE DIFUSION Y EXTENSION UNIVERSITARIA', 'DIRECCION DE DIFUSION Y EXTENSION UNIVERSITARIA', 'cuervo.png', 0, 12, '2023-03-01 01:36:28', '2023-03-01 01:36:30'),
+(12, '12', 'DIRECCION DE DIFUSION Y EXTENSION UNIVERSITARIA', 'DIRECCION DE DIFUSION Y EXTENSION UNIVERSITARIA', 'cuervo.png', 1, 12, '2023-03-01 01:36:28', '2023-03-07 07:01:00'),
 (13, '13', 'DEPARTAMENTO DE ACTIVIDADES CULTURALES Y DEPOSTIVA', 'DEPARTAMENTO DE ACTIVIDADES CULTURALES Y DEPOSTIVAS ', 'cuervo.png', 0, 13, '2023-03-01 01:39:31', '2023-03-01 01:39:33'),
 (14, '14', 'DEPARTAMENTO DE PRENSA Y DIFUSION', 'DEPARTAMENTO DE PRENSA Y DIFUSION', 'cuervo.png', 0, 14, '2023-03-01 01:40:20', '2023-03-01 01:40:23'),
 (15, '15', 'DEPARTAMENTO DE EDUCACION CONTINUA', 'DEPARTAMENTO DE EDUCACION CONTINUA', 'cuervo.png', 0, 15, '2023-03-01 01:41:00', '2023-03-01 01:41:03'),
-(16, '16', 'DIRECCION DE ADMINISTRACION Y FINANZAS', 'DIRECCION DE ADMINISTRACION Y FINANZAS', 'cuervo.png', 0, 16, '2023-03-01 01:41:40', '2023-03-01 01:41:42'),
-(17, '17', 'DEPARTAMENTO DE RECURSOS HUMANOS', 'DEPARTAMENTO DE RECURSOS HUMANOS', 'cuervo.png	', 0, 17, '2023-03-01 01:42:17', '2023-03-01 01:42:19'),
+(16, '16', 'DIRECCION DE ADMINISTRACION Y FINANZAS', 'DIRECCION DE ADMINISTRACION Y FINANZAS', 'cuervo.png', 1, 16, '2023-03-01 01:41:40', '2023-03-07 02:41:19'),
+(17, '17', 'DEPARTAMENTO DE RECURSOS HUMANOS', 'DEPARTAMENTO DE RECURSOS HUMANOS', '20230306203711rectoria.jpeg', 1, 17, '2023-03-01 01:42:17', '2023-03-07 02:37:11'),
 (18, '18', 'DEPARTAMENTO DE RECURSOS MATERIALES', 'DEPARTAMENTO DE RECURSOS MATERIALES', 'cuervo.png', 0, 18, '2023-03-01 01:43:30', '2023-03-01 01:43:31'),
 (19, '19', 'DEPARTAMENTO DE MANTENIMIENTO Y SERVICIOS GENERALE', 'DEPARTAMENTO DE MANTENIMIENTO Y SERVICIOS GENERALES', 'cuervo.png', 0, 19, '2023-03-01 01:44:17', '2023-03-01 01:44:18'),
 (20, '20', 'DEPARTAMENTO DE SISTEMAS', 'DEPARTAMENTO DE SISTEMAS', 'cuervo.png', 0, 20, '2023-03-01 01:44:57', '2023-03-01 01:45:03'),
-(21, '21', 'SUBBDIRECCION DE FINANZAS', 'SUBBDIRECCION DE FINANZAS', 'cuervo.png', 0, 21, '2023-03-01 01:45:55', '2023-03-01 01:45:57');
+(21, '21', 'SUBBDIRECCION DE FINANZAS', 'SUBBDIRECCION DE FINANZAS', 'cuervo.png', 0, 21, '2023-03-01 01:45:55', '2023-03-01 01:45:57'),
+(22, '22', 'HOLIS', 'GRACIAS', 'cuervo.png', 0, 1, '2023-03-07 02:30:54', '2023-03-07 04:53:28');
 
 -- --------------------------------------------------------
 
@@ -208,6 +209,13 @@ CREATE TABLE `tb_metas` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `tb_metas`
+--
+
+INSERT INTO `tb_metas` (`id_meta`, `clave`, `nombre`, `descripcion`, `activo`, `id_registro`, `created_at`, `updated_at`) VALUES
+(2, 'MET1', 'Entrega de jabón', 'Para la limpieza de aulas', 0, 1, '2023-03-07 02:07:48', '2023-03-07 02:10:03');
+
 -- --------------------------------------------------------
 
 --
@@ -230,10 +238,11 @@ CREATE TABLE `tb_programas` (
 --
 
 INSERT INTO `tb_programas` (`id_programa`, `abreviatura`, `nombre`, `descripcion`, `activo`, `id_registro`, `created_at`, `updated_at`) VALUES
-(1, 'SIPREP', 'SISTEMA DE PLANEACION Y PRESUPUESTO', 'ENCARGADO DEL PRESUPUESTO	\r\n', 0, 1, '2023-03-01 01:18:09', '2023-03-01 01:18:12'),
+(1, 'SIPREP', 'SISTEMA DE PLANEACION Y PRESUPUESTO', 'ENCARGADO DEL PRESUPUESTO', 1, 1, '2023-03-01 01:18:09', '2023-03-07 07:25:01'),
 (2, 'POA FEDERAL', 'PROGRAMA OPERATIVO ANUAL  FEDERAL', 'PRESUPUESTAR LOS RECURSOS FINANCIEROSN NECESARIOS XD', 0, 2, '2023-03-01 01:19:27', '2023-03-01 01:19:30'),
-(3, 'PIDE', 'PLATAFORMA DE INTERINOS DOCENTEES DE EXTREMADURA P', ' referencia a Programa Intensivo de Dirección de Empresas de forma abreviada', 0, 3, '2023-03-01 01:22:12', '2023-03-01 01:22:15'),
-(4, 'PROPIAS', 'UTVT', 'PROGRMA DE LA UTVT\r\n', 0, 4, '2023-03-01 01:23:20', '2023-03-01 01:23:23');
+(3, 'PIDE', 'PLATAFORMA DE INTERINOS DOCENTES DE EXTREMADURA', 'Referencia a Programa Intensivo de Dirección de Empresas de forma abreviada', 1, 3, '2023-03-01 01:22:12', '2023-03-07 07:26:24'),
+(4, 'PROPIAS', 'UTVT', 'PROGRMA DE LA UTVT\r\n', 0, 4, '2023-03-01 01:23:20', '2023-03-01 01:23:23'),
+(5, 'Irure est vel eum de', 'Rerum libero ut esse', 'Pariatur Corporis o', 1, NULL, '2023-03-07 07:27:35', '2023-03-07 07:27:35');
 
 -- --------------------------------------------------------
 
@@ -257,7 +266,10 @@ CREATE TABLE `tb_tipos` (
 --
 
 INSERT INTO `tb_tipos` (`id`, `clave`, `nombre`, `descripcion`, `activo`, `id_registro`, `created_at`, `updated_at`) VALUES
-(1, 'TIP1', 'Administardor', 'Contiene todos los permisos sobre el sistema', 1, 1, '2023-03-04 00:17:17', '2023-03-04 00:17:17');
+(1, 'TIP1', 'Administardor', 'Contiene todos los permisos sobre el sistema', 0, 1, '2023-03-04 00:17:17', '2023-03-07 07:37:47'),
+(2, 'TIP1', 'Encargado de Área', 'Director o similar', 1, 1, '2023-03-06 22:53:40', '2023-03-06 22:53:40'),
+(3, 'TIP3', 'Secretaria', 'No posee los permisos que le director', 1, 1, '2023-03-06 22:59:01', '2023-03-06 22:59:01'),
+(4, 'TIP4', 'Invitado', 'No tiene la mayoría de permisos', 0, 1, '2023-03-07 07:38:42', '2023-03-07 07:40:39');
 
 -- --------------------------------------------------------
 
@@ -289,7 +301,11 @@ CREATE TABLE `tb_usuarios` (
 --
 
 INSERT INTO `tb_usuarios` (`id_usuario`, `clave`, `nombre`, `app`, `apm`, `gen`, `fn`, `academico`, `foto`, `email`, `pass`, `id_tipo`, `activo`, `id_registro`, `created_at`, `updated_at`) VALUES
-(1, 'USR1', 'Jimena', 'Diaz', 'De Los Santos', 'F', '2003-02-24', 'TSU Area desarrollo de software multiplataforma', 'cuervo.png', 'al222110707@gmail.com', '123456', 1, 1, 1, '2023-03-04 00:18:34', '2023-03-04 00:18:34');
+(1, 'USR1', 'Jimena', 'Diaz', 'De Los Santos', 'F', '2003-02-24', 'TSU Area desarrollo de software multiplataforma', 'cuervo.png', 'al222110707@gmail.com', '123456', 1, 1, 1, '2023-03-04 00:18:34', '2023-03-07 08:18:26'),
+(2, 'USR2', 'Jossue', 'Candelas', 'Hernandez', 'M', '2003-06-06', 'TSU Desarrollo de software multiplataforma', '20230306165445imagen_uippe2.png', 'al222010230@gmail.com', '123123', 2, 0, 1, '2023-03-06 22:54:46', '2023-03-07 04:57:11'),
+(3, 'USR3', 'Eduardo', 'Diaz', 'Perez', 'M', '1978-02-01', 'TSU Desarrollo de software multiplataforma', '20230306165955cropped-logo-utvt.png', 'lalo@gmail.com', '123123', 3, 1, 1, '2023-03-06 22:59:55', '2023-03-07 07:57:07'),
+(4, 'USR4', 'Leopoldo', 'Diaz', 'De Los Santos', 'M', '2003-06-05', 'TSU Desarrollo de software multiplataforma', '20230306214257rectoria.jpeg', 'leo@gmail.com', '123123', 3, 1, 1, '2023-03-07 01:58:42', '2023-03-07 03:42:57'),
+(5, 'URS5', 'Francis', 'Carney', 'Jacobs', 'M', '1991-08-01', 'TSU Desarrollo de software multiplataforma', 'cuervo.png', 'suvikubyt@mailinator.com', '123123', 1, 1, 1, '2023-03-07 03:26:09', '2023-03-07 03:26:09');
 
 -- --------------------------------------------------------
 
@@ -422,7 +438,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT de la tabla `tb_areas`
 --
 ALTER TABLE `tb_areas`
-  MODIFY `id_area` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_area` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_areasmetas`
@@ -446,25 +462,25 @@ ALTER TABLE `tb_calendarizars`
 -- AUTO_INCREMENT de la tabla `tb_metas`
 --
 ALTER TABLE `tb_metas`
-  MODIFY `id_meta` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_meta` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_programas`
 --
 ALTER TABLE `tb_programas`
-  MODIFY `id_programa` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_programa` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_tipos`
 --
 ALTER TABLE `tb_tipos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_usuarios`
 --
 ALTER TABLE `tb_usuarios`
-  MODIFY `id_usuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
